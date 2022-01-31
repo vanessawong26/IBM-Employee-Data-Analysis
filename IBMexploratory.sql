e1d8777e1b6ae117fb030be581a57a27f2185986
Vanessa Wong <vwong626@gmail.com>
2022-01-30 19:26:08 -0500

    Update

--- IBMexploratory.sql
+++ IBMexploratory.sql
@@ -19,7 +19,7 @@ AVG(JobInvolvement) avg_job_involve,
 AVG(WorkLifeBalance) avg_work_life
 FROM vw_personal.IBM_employee_data
 GROUP BY JobRole
-ORDER BY avg_job_satis, avg_job_involve, avg_work_life DESC;
+ORDER BY avg_job_satis, avg_job_involve, avg_work_life;
 
 -- Gender pay gap?
 -- With the exception of the Manager, Research Director, and Sales Executive (3/9 job roles), females on average earn a higher monthly income than their male Job Role counterparts.
